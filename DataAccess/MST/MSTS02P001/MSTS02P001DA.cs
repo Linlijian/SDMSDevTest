a4dd8844705a869cdc1a8b4895989a5b84f9d609
po.klinmala <praw__Rul2__lvluk@outlook.co.th>
2018-12-24 15:35:18 +0700

    #44 add MSTS02P001 [template]

---
+++ DataAccess/MST/MSTS02P001/MSTS02P001DA.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using UtilityLib;
+
+namespace DataAccess.MST
+{
+    public class MSTS02P001DA : BaseDA
+    {
+        public MSTS02P001DTO DTO { get; set; }
+
+        #region ====Property========
+        public MSTS02P001DA()
+        {
+            DTO = new MSTS02P001DTO();
+        }
+
+        #endregion
+
+        #region ====Select==========
+        protected override BaseDTO DoSelect(BaseDTO baseDTO)
+        {
+            var dto = (MSTS02P001DTO)baseDTO;
+            switch (dto.Execute.ExecuteType)
+            {
+                case MSTS02P001ExecuteType.GetAll: return GetAll(dto);
+                case MSTS02P001ExecuteType.GetByID: return GetByID(dto);
+            }
+            return dto;
+        }
+
+        private MSTS02P001DTO GetAll(MSTS02P001DTO dto)
+        {
+
+            return dto;
+        }
+        private MSTS02P001DTO GetByID(MSTS02P001DTO dto)
+        {
+
+            return dto;
+        }
+        #endregion
+
+        #region ====Insert==========
+        protected override BaseDTO DoInsert(BaseDTO baseDTO)
+        {
+            var dto = (MSTS02P001DTO)baseDTO;
+            switch (dto.Execute.ExecuteType)
+            {
+                case MSTS02P001ExecuteType.Insert: return Insert(dto);
+
+            }
+            return dto;
+        }
+        private MSTS02P001DTO Insert(MSTS02P001DTO dto)
+        {
+
+            return dto;
+        }
+        #endregion
+
+        #region ====Update==========
+
+        protected override BaseDTO DoUpdate(BaseDTO baseDTO)
+        {
+            var dto = (MSTS02P001DTO)baseDTO;
+            switch (dto.Execute.ExecuteType)
+            {
+                case MSTS02P001ExecuteType.Update: return Update(dto);
+            }
+            return dto;
+        }
+        private MSTS02P001DTO Update(MSTS02P001DTO dto)
+        {
+
+            return dto;
+        }
+        #endregion
+
+        #region ====Delete==========
+        protected override BaseDTO DoDelete(BaseDTO baseDTO)
+        {
+            var dto = (MSTS02P001DTO)baseDTO;
+            if (dto.Models.Count() > 0)
+            {
+                foreach (var item in dto.Models)
+                {
+
+                }
+            }
+
+            return dto;
+        }
+        #endregion
+    }
+}
---
+++ DataAccess/MST/MSTS02P001/MSTS02P001Model.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using FluentValidation.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.MST
+{
+    [Validator(typeof(MSTS02P001Validator))]
+    [Serializable]
+    public class MSTS02P001Model : StandardModel
+    {
+
+        [Display(Name = "ISSUE_TYPE", ResourceType = typeof(Translation.MST.MSTS02P001))]
+        public string ISSUE_TYPE { get; set; }
+        [Display(Name = "TYPE_RATE", ResourceType = typeof(Translation.MST.MSTS02P001))]
+        public string TYPE_RATE { get; set; }
+        [Display(Name = "MAN_PLM_SA", ResourceType = typeof(Translation.MST.MSTS02P001))]
+        public decimal? MAN_PLM_SA { get; set; }
+        [Display(Name = "MAN_PLM_QA", ResourceType = typeof(Translation.MST.MSTS02P001))]
+        public decimal? MAN_PLM_QA { get; set; }
+        [Display(Name = "MAN_PLM_PRG", ResourceType = typeof(Translation.MST.MSTS02P001))]
+        public decimal? MAN_PLM_PRG { get; set; }
+
+    }
+    public class MSTS02P001Validator : AbstractValidator<MSTS02P001Model>
+    {
+        public MSTS02P001Validator()
+        {
+            RuleSet("Add", () =>
+            {
+                Valid();
+            });
+            RuleSet("Edit", () =>
+            {
+                Valid();
+            });
+        }
+
+        private void Valid()
+        {
+            //RuleFor(t => t.TYPE_DAY).NotEmpty();
+        }
+    }
+}
