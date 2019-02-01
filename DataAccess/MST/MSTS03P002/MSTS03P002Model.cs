6229082ae57fdb9b3b28eb965bf9005f726a27b7
po.klinmala <praw__Rul2__lvluk@outlook.co.th>
2019-02-01 13:20:54 +0700

    #80 add MSTS03P002

---
+++ DataAccess/MST/MSTS03P002/MSTS03P002Model.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation;
+using FluentValidation.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.MST
+{
+    [Validator(typeof(MSTS03P002Validator))]
+    [Serializable]
+    public class MSTS03P002Model : StandardModel
+    {
+        [Display(Name = "PRIORITY_NAME", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public string PRIORITY_NAME { get; set; }
+        [Display(Name = "ISSUE_TYPE", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public string ISSUE_TYPE { get; set; }
+        [Display(Name = "RES_TIME", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public decimal? RES_TIME { get; set; }
+        [Display(Name = "T_RES_TIME", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public decimal? T_RES_TIME { get; set; }
+        [Display(Name = "REMASK", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public string REMASK { get; set; }
+        [Display(Name = "IS_FREE", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public string IS_FREE { get; set; }
+
+        [Display(Name = "KEY_ID", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public string KEY_ID { get; set; }
+        public IEnumerable<DDLCenterModel> KEY_ID_MODEL { get; set; }
+        [Display(Name = "T_RES_TYPE", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public string T_RES_TYPE { get; set; }
+        public IEnumerable<DDLCenterModel> T_RES_TYPE_MODEL { get; set; }
+        [Display(Name = "RES_TYPE", ResourceType = typeof(Translation.MST.MSTS03P002))]
+        public string RES_TYPE { get; set; }
+        public IEnumerable<DDLCenterModel> RES_TYPE_MODEL { get; set; }
+
+        public decimal? PIT_ID { get; set; }
+        public bool IS_USED { get; set; }
+    }
+    public class MSTS03P002Validator : AbstractValidator<MSTS03P002Model>
+    {
+        public MSTS03P002Validator()
+        {
+            RuleSet("Add", () =>
+            {
+                Valid();
+                RuleFor(t => t.KEY_ID).NotEmpty();
+            });
+            RuleSet("Edit", () =>
+            {
+                Valid();
+            });
+        }
+        
+        private void Valid()
+        {
+            RuleFor(m => m.PRIORITY_NAME).Store("CD_MSTS03P002_001", m => m.ISSUE_TYPE).NotEmpty();
+            RuleFor(m => m.ISSUE_TYPE).Store("CD_MSTS03P002_001", m => m.PRIORITY_NAME).NotEmpty();
+            RuleFor(t => t.RES_TIME).NotEmpty().GreaterThanOrEqualTo(0).LessThanOrEqualTo(Convert.ToDecimal(99.9)).WithMessage(Translation.CenterLang.Validate.OneNumber2Digit1);
+            RuleFor(t => t.T_RES_TIME).NotEmpty().GreaterThanOrEqualTo(0).LessThanOrEqualTo(Convert.ToDecimal(99.9)).WithMessage(Translation.CenterLang.Validate.OneNumber2Digit1);
+            RuleFor(t => t.T_RES_TYPE).NotEmpty();
+            RuleFor(t => t.RES_TYPE).NotEmpty();
+        }
+    }
+}
