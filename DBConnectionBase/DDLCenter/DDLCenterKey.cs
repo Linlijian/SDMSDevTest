01807473e7736f23796f635c2e30662ae52538a0
po.klinmala <praw__Rul2__lvluk@outlook.co.th>
2019-03-23 13:55:13 +0700

    #138 add DDLCenterKey

--- DBConnectionBase/DDLCenter/DDLCenterKey.cs
+++ DBConnectionBase/DDLCenter/DDLCenterKey.cs
@@ -1,22 +1,23 @@
-﻿ 
+﻿
 
 
 namespace DataAccess
 {
-	public static class DDLCenterKey
-	{
+    public static class DDLCenterKey
+    {
         public const string DD_VSMS_FIX_TYPEDATE = "DD_VSMS_FIX_TYPEDATE";
         public const string DD_VSMS_COMPANY_001 = "DD_VSMS_COMPANY_001";
-		public const string DD_VSMS_DEPARTMENT_001 = "DD_VSMS_DEPARTMENT_001";
-		public const string DD_VSMS_FIX_OPTIONDETAIL_002 = "DD_VSMS_FIX_OPTIONDETAIL_002";
-		public const string DD_VSMS_FIX_PROVINCE_EN_001 = "DD_VSMS_FIX_PROVINCE_EN_001";
-		public const string DD_VSMS_FIX_PROVINCE_TH_001 = "DD_VSMS_FIX_PROVINCE_TH_001";
-		public const string DD_VSMS_SYSTEM_001 = "DD_VSMS_SYSTEM_001";
-		public const string DD_VSMS_TITLE_001 = "DD_VSMS_TITLE_001";
-		public const string DD_VSMS_USRGROUP_001 = "DD_VSMS_USRGROUP_001";
-		public const string DD_VSMS_USRGROUP_002 = "DD_VSMS_USRGROUP_002";
-		public const string DD_VSMS_USRGROUP_003 = "DD_VSMS_USRGROUP_003";
-		public const string DD_VSMS_USRGROUP_004 = "DD_VSMS_USRGROUP_004";
+        public const string DD_VSMS_SYSTEM_002 = "DD_VSMS_SYSTEM_002";
+        public const string DD_VSMS_DEPARTMENT_001 = "DD_VSMS_DEPARTMENT_001";
+        public const string DD_VSMS_FIX_OPTIONDETAIL_002 = "DD_VSMS_FIX_OPTIONDETAIL_002";
+        public const string DD_VSMS_FIX_PROVINCE_EN_001 = "DD_VSMS_FIX_PROVINCE_EN_001";
+        public const string DD_VSMS_FIX_PROVINCE_TH_001 = "DD_VSMS_FIX_PROVINCE_TH_001";
+        public const string DD_VSMS_SYSTEM_001 = "DD_VSMS_SYSTEM_001";
+        public const string DD_VSMS_TITLE_001 = "DD_VSMS_TITLE_001";
+        public const string DD_VSMS_USRGROUP_001 = "DD_VSMS_USRGROUP_001";
+        public const string DD_VSMS_USRGROUP_002 = "DD_VSMS_USRGROUP_002";
+        public const string DD_VSMS_USRGROUP_003 = "DD_VSMS_USRGROUP_003";
+        public const string DD_VSMS_USRGROUP_004 = "DD_VSMS_USRGROUP_004";
         public const string DD_VSMS_FIX_KEY_ID = "DD_VSMS_FIX_KEY_ID";
         public const string DD_VSMS_FIX_TYPETIME = "DD_VSMS_FIX_TYPETIME";
         public const string DD_VSMS_ISSUETYPE = "DD_VSMS_ISSUETYPE";
@@ -25,6 +26,11 @@ namespace DataAccess
         public const string DD_MISS01P001_002 = "DD_MISS01P001_002";
         public const string DD_MISS01P001_003 = "DD_MISS01P001_003";
         public const string DD_MISS01P001_004 = "DD_MISS01P001_004";
-
+        public const string DD_MISS01P002_001 = "DD_MISS01P002_001";
+        public const string DD_MISS01P002_002 = "DD_MISS01P002_002";
+        public const string DD_MISS01P002_003 = "DD_MISS01P002_003";
+        public const string DD_APPLICATION = "DD_APPLICATION";
+        public const string DD_MISS01P003_001 = "DD_MISS01P003_001";
+        public const string DD_SECS01P001_001 = "DD_SECS01P001_001";
     }
 }
